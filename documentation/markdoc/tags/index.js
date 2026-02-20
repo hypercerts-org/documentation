8982de3098002b3970d1f46ac5740773d0854921
daviddao <david@gainforest.net>
2026-02-20 19:06:22 +0800

    Fix hydration mismatch: add tags barrel file and fix HeroBanner nesting (docs-y5x.4)

---
+++ documentation/markdoc/tags/index.js
@@ -0,0 +1,17 @@
+import callout from './callout.markdoc';
+import columns from './columns.markdoc';
+import column from './column.markdoc';
+import figure from './figure.markdoc';
+import cardLink from './card-link.markdoc';
+import cardGrid from './card-grid.markdoc';
+import heroBanner from './hero-banner.markdoc';
+
+export default {
+  callout,
+  columns,
+  column,
+  figure,
+  'card-link': cardLink,
+  'card-grid': cardGrid,
+  'hero-banner': heroBanner,
+};
